9fa4579d6c6001cfc59ff772c325263f12d53707
parsa3323 <mehr2.business@gmail.com>
2025-12-17 20:42:56 +0330

    better lore

--- armorstand-plugin/src/main/java/com/parsa3323/aas/inventory/LeftLegItem.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/inventory/LeftLegItem.java
@@ -19,6 +19,7 @@
 package com.parsa3323.aas.inventory;
 
 import com.parsa3323.aas.inventory.manager.InventoryItem;
+import com.parsa3323.aas.utils.ColorUtils;
 import com.parsa3323.aas.utils.VersionSupportUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.ArmorStand;
@@ -44,8 +45,8 @@ public class LeftLegItem extends InventoryItem {
         lore.add(ChatColor.YELLOW + "LEFT CLICK " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY + "Rotate left leg to left");
         lore.add(ChatColor.YELLOW + "SHIFT + RIGHT CLICK " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY + "Move left leg up");
         lore.add(ChatColor.YELLOW + "SHIFT + LEFT CLICK " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY + "Move left leg down");
-        lore.add(ChatColor.DARK_GRAY + "AdvancedArmorStands Editor Item");
-
+        lore.add("");
+        lore.add(ColorUtils.boldAndColor(ChatColor.DARK_GRAY) + "AdvancedArmorStands Editor Item");
 
         ItemStack itemStack = VersionSupportUtil.getVersionSupport().getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDA4NWE2NzgwMmMwZWE0NjJmYmJhNGExZGJiNjgxNzQ4ZjVjZDAxYTMwMzQ3NjdiMWVjZjgzYWUxMjdlODUwZiJ9fX0=");
         ItemMeta itemMeta = itemStack.getItemMeta();
