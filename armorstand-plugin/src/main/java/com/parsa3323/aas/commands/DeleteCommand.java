0eb150fa8e2ac051bf32160bc01334b48bf66352
parsa3323 <mehr2.business@gmail.com>
2026-01-10 20:34:21 +0330

    add "all" options

--- armorstand-plugin/src/main/java/com/parsa3323/aas/commands/DeleteCommand.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/commands/DeleteCommand.java
@@ -82,7 +82,7 @@ public class DeleteCommand extends SubCommand {
             return;
         }
 
-        player.sendMessage(ChatColor.GREEN + "Deleted ");
+        player.sendMessage(ChatColor.GREEN + "Deleted ArmorStand: " + args[1]);
 
     }
 
