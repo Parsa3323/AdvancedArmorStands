fc7493ef11484a74f8c7c4c9c6f6b8e7dc51eb91
parsa3323 <mehr2.business@gmail.com>
2026-01-11 18:33:36 +0330

    okkk

--- armorstand-plugin/src/main/java/com/parsa3323/aas/options/BasePlateOption.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/options/BasePlateOption.java
@@ -19,6 +19,8 @@
 package com.parsa3323.aas.options;
 
 import com.parsa3323.aas.options.manager.SettingsOption;
+import com.parsa3323.aas.utils.ColorUtils;
+import com.parsa3323.aas.utils.IconUtils;
 import com.parsa3323.aas.utils.VersionSupportUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.ArmorStand;
@@ -44,7 +46,7 @@ public class BasePlateOption extends SettingsOption {
         lore.add(ChatColor.GRAY + "Enable and disable");
         lore.add(ChatColor.GRAY + "base plate for this ArmorStand ");
         lore.add("");
-        lore.add((armorStand.hasBasePlate()) ? ChatColor.YELLOW + "✔ Has base plate" : ChatColor.RED + "✘ Doesn't have base plate");
+        lore.add((armorStand.hasBasePlate()) ? ColorUtils.boldAndColor(ChatColor.GOLD) + IconUtils.CHECK() + ChatColor.YELLOW + " Has base plate" : ColorUtils.boldAndColor(ChatColor.DARK_RED) + IconUtils.CROSS() + ChatColor.RED + " Doesn't have base plate");
 
 
         itemMeta.setLore(lore);
