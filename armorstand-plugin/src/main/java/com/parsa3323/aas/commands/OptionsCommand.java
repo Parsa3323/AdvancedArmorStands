4af629516a50a4ef69d62c9e4849a9d3370c464b
parsa3323 <mehr2.business@gmail.com>
2026-01-11 12:24:23 +0330

    fixed somethings

--- armorstand-plugin/src/main/java/com/parsa3323/aas/commands/OptionsCommand.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/commands/OptionsCommand.java
@@ -62,9 +62,9 @@ public class OptionsCommand extends SubCommand {
         if (null == armorStand) {
             String suggestion = getClosest(args[1], ArmorStandUtils.getArmorStandList());
             if (suggestion != null) {
-                player.sendMessage(ChatColor.RED + "Invalid armor stand '" + args[1] + "'. Did you mean '" + suggestion + "'?");
+                player.sendMessage(ChatColor.RED + "Invalid ArmorStand '" + args[1] + "'. Did you mean '" + suggestion + "'?");
             } else {
-                player.sendMessage(ChatColor.RED + "Invalid armor stand");
+                player.sendMessage(ChatColor.RED + "Invalid ArmorStand");
             }
             return;
         }
