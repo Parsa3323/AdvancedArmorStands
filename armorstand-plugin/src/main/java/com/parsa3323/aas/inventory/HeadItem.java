178c319fbb739c34d2a0cf572517ee54c3a6d1f6
parsa3323 <mehr2.business@gmail.com>
2026-01-26 20:14:33 +0330

    this

--- armorstand-plugin/src/main/java/com/parsa3323/aas/inventory/HeadItem.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/inventory/HeadItem.java
@@ -46,7 +46,7 @@ public class HeadItem extends InventoryItem {
         lore.add(ChatColor.YELLOW + "SHIFT + RIGHT CLICK " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY + "Move head up");
         lore.add(ChatColor.YELLOW + "SHIFT + LEFT CLICK " + ChatColor.DARK_GRAY + "» " + ChatColor.GRAY + "Move head down");
         lore.add("");
-        lore.add(ColorUtils.boldAndColor(ChatColor.DARK_GRAY) + "AdvancedArmorStands Editor Item");
+        lore.add(ColorUtils.italicAndColor(ChatColor.DARK_GRAY) + "AdvancedArmorStands Editor Item");
 
         ItemStack itemStack = VersionSupportUtil.getVersionSupport().getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTJmNTA5MjUwZWM0YTExMTZjMWE1ZmE3Y2E5YTNiNDFkZTI1YWYwNjdmMGNkNmNlN2VmN2ViMTI2OTc0ZWFlYyJ9fX0=");
         ItemMeta itemMeta = itemStack.getItemMeta();
