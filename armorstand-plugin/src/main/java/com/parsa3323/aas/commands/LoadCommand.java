ec91c56d8db47f3469a4c28bf88650331c8cb1ce
parsa3323 <mehr2.business@gmail.com>
2026-05-13 17:50:30 +0330

    ...

--- armorstand-plugin/src/main/java/com/parsa3323/aas/commands/LoadCommand.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/commands/LoadCommand.java
@@ -51,7 +51,6 @@ public class LoadCommand extends SubCommand {
             }
         }
 
-
         lore.add(ColorUtils.boldAndColor(ChatColor.YELLOW) + "/as load " + TextUtils.getFirstContent(unloaded, "unloadedArmorStand"));
         lore.add(ColorUtils.boldAndColor(ChatColor.YELLOW) + "/as load --all");
         return lore;
