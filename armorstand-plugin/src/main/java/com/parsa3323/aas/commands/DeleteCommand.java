8891805ceaca91179a0f4b6ee1c029719d9413c4
parsa3323 <mehr2.business@gmail.com>
2026-05-19 23:17:26 +0330

    ...

--- armorstand-plugin/src/main/java/com/parsa3323/aas/commands/DeleteCommand.java
+++ armorstand-plugin/src/main/java/com/parsa3323/aas/commands/DeleteCommand.java
@@ -79,7 +79,7 @@ public class DeleteCommand extends SubCommand {
             return;
         }
 
-        if (args[2].equalsIgnoreCase("--full")) {
+        if (args.length > 2 && args[2].equalsIgnoreCase("--full")) {
             ArmorStandUtils.deleteArmorStand(args[1]);
             ArmorStandsConfig.get().set("armorstands." + args[1], null);
             ArmorStandsConfig.save();
